4970a23acb04c709aaa0124462d1b068f5f7bb66
IanDavis742 <iandavis742@gmail.com>
2019-02-09 16:10:55 -0500

    Working code as of 2/9/2019

---
+++ src/main/java/frc/robot/Lift.java
@@ -0,0 +1,167 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+/**
+ * Lifting mechanism on the robot
+ */
+public class Lift {
+    boolean liftEngaged;
+    WPI_TalonSRX frontLiftMotor;
+    WPI_TalonSRX backLiftMotor;
+    WPI_VictorSPX leftDriveMotor;
+    WPI_VictorSPX rightDriveMotor;
+    SpeedControllerGroup liftMotors;
+    DoubleSolenoid frontLiftBrake;
+    DoubleSolenoid backLiftBrake;
+
+    public Lift(int idFrontLiftMotor, int idBackLiftMotor, int idLeftDriveMotor, int idRightDriveMotor) {
+        frontLiftMotor = new WPI_TalonSRX(idFrontLiftMotor);
+        backLiftMotor = new WPI_TalonSRX(idBackLiftMotor);
+        backLiftMotor.setInverted(true);
+        leftDriveMotor= new WPI_VictorSPX(idLeftDriveMotor);
+        rightDriveMotor= new WPI_VictorSPX(idRightDriveMotor);
+        liftMotors = new SpeedControllerGroup(frontLiftMotor, backLiftMotor);
+        // frontLiftBrake = new DoubleSolenoid(0, 0);
+        // backLiftBrake = new DoubleSolenoid(0, 0);
+        leftDriveMotor.follow(rightDriveMotor);
+    }
+    
+    public void liftRobotUp(double speed) { //Push the robot up by putting both sets of wheels down
+        liftEngaged = true;
+        new Thread() {
+            public void run() {
+                frontBrakeDisengage();
+                backBrakeDisengage();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) { }
+                liftMotors.set(1);
+                frontBrakeEngage();
+                backBrakeEngage();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) { }
+            }
+        }.start();
+    }
+    public void robotDown() { //Bring both wheels back in at the same time, lowering the bot
+        liftEngaged = false;
+        new Thread() {
+            public void run() {
+                frontBrakeDisengage();
+                backBrakeDisengage();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) { }
+                liftMotors.set(-1);
+                frontBrakeEngage();
+                backBrakeEngage();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) { }
+            }
+        }.start();
+    }   
+    
+    public void frontPostDown(double speed) { //Put down only this set of wheels
+        new Thread() {
+            public void run() {
+                frontBrakeDisengage();
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) { }
+                frontLiftMotor.set(-speed);
+                frontBrakeEngage();
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) { }
+            }
+        }.start();
+    }
+
+    public void frontPostUp(double speed) { //Bring back up only this set of wheels
+        new Thread() {
+            public void run() {
+                frontBrakeDisengage();
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) { }
+                frontLiftMotor.set(speed);
+                frontBrakeEngage();
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) { }
+            }
+        }.start();
+    }
+
+    public void backPostDown(double speed) { //Put down only this set of wheels
+        liftEngaged = true;
+        new Thread() {
+            public void run() {
+                backBrakeDisengage();
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) { }
+                backLiftMotor.set(-speed);
+                backBrakeEngage();
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) { }
+            }
+        }.start();
+    }
+    
+    public void backPostUp(double speed) { //Bring back up only this set of wheels
+        liftEngaged = false;
+        new Thread() {
+            public void run() {
+                backBrakeDisengage();
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) { }
+                backLiftMotor.set(speed);
+                backBrakeEngage();
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) { }
+            }
+        }.start();
+    }
+
+    public void drive(double speed) {
+        if(liftEngaged) {
+            leftDriveMotor.set(ControlMode.PercentOutput, speed); // Controls both this motor and the rightDriveMotor
+        }
+    }
+
+    public void frontBrakeEngage() {
+        // frontLiftBrake.set(Value.kForward);
+    }
+
+    public void frontBrakeDisengage() {
+        // frontLiftBrake.set(Value.kReverse);
+    }
+
+    public void backBrakeEngage() {
+        // backLiftBrake.set(Value.kForward);
+    }
+
+    public void backBrakeDisengage() {
+        // backLiftBrake.set(Value.kReverse);
+    }
+}
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,257 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends IterativeRobot {
+
+  DifferentialDrive drive;
+  WPI_TalonSRX driveMotor1;
+  WPI_TalonSRX driveMotor2;
+  WPI_TalonSRX driveMotor3;
+  WPI_TalonSRX driveMotor4;
+  SpeedControllerGroup leftDrive;
+  SpeedControllerGroup rightDrive;
+  VictorSP winchMotor;
+  Encoder winchEncoder;
+  Lift lift;
+  Compressor compressor;
+  DoubleSolenoid intake;
+  DoubleSolenoid intakeSlider;
+  DigitalInput elevatorTopSwitch;
+  DigitalInput elevatorBottomSwitch;
+  XboxController controller;
+  VisionProcessing vision;
+  PIDController pidController;
+
+  /**
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // 1 victor sp, and 2 victor spxes and 6 talon srxes
+    // winchMotor and the two lift motors NEED to be TalonSRX
+    // Leaving 3 TalonSRX, 1 VictorSP, and 2 VictorSPX
+    // Drive will be 3 talon, 1 spx
+    // Leaving the liftDrive to sp and spx
+    driveMotor1 = new WPI_TalonSRX(1);
+    driveMotor1.set(ControlMode.PercentOutput, 0);
+    driveMotor2 = new WPI_TalonSRX(2);
+    driveMotor2.set(ControlMode.PercentOutput, 0);
+    driveMotor3 = new WPI_TalonSRX(3);
+    driveMotor3.set(ControlMode.PercentOutput, 0);
+    driveMotor4 = new WPI_TalonSRX(4);
+    driveMotor4.set(ControlMode.PercentOutput, 0);
+    leftDrive = new SpeedControllerGroup(driveMotor1, driveMotor2);
+    rightDrive = new SpeedControllerGroup(driveMotor3, driveMotor4);
+    drive = new DifferentialDrive(leftDrive, rightDrive);
+    lift = new Lift(5, 6, 7, 8);
+    winchMotor = new VictorSP(0);
+    winchMotor.setInverted(true);
+    winchEncoder= new Encoder(1, 2);
+    compressor = new Compressor(0);
+    // intake = new DoubleSolenoid(,);
+    // intakeSlider = new DoubleSolenoid(,);
+    elevatorTopSwitch = new DigitalInput(8);
+    elevatorBottomSwitch = new DigitalInput(9);
+    controller = new XboxController(0);
+    pidController = new PIDController(.7, 0, 0, winchEncoder, winchMotor);
+    pidController.setSetpoint(1.0);
+    pidController.setPercentTolerance(15.0);
+    // vision = new VisionProcessing();
+    compressor.start();
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the switch structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+    /*
+    To get d-pad: controller.getPov(); where:
+    Up: 0
+    Right: 90
+    Down: 180
+    Left: 270
+
+    Controls as of 2/9/19
+    Left Joystick for drive - done
+    Left trigger both lift down - implemented
+    Right trigger back-lift down - implemented
+    A for front-lift up - implemented
+    B for back lift up - implemented
+    Bumpers for elevator - implemented
+    D-pad up for intake open - implemented
+    D-pad down for intake close - implemented
+    D-pad left for intake left
+    D-pad right for intake out
+    Start for start line tracking - Just align, then turn off
+    Back for off
+    Right joystick for second drive, but only when the lift is down (engaged)
+
+    */
+    double rightTrigger = controller.getTriggerAxis(Hand.kRight);
+    double leftTrigger = controller.getTriggerAxis(Hand.kLeft);
+    double rightJoyStickY = controller.getY(Hand.kRight);
+    int dPad = controller.getPOV(0);
+
+    drive.arcadeDrive(controller.getY(Hand.kLeft), controller.getX(Hand.kLeft));
+    
+    if(controller.getAButton()) {
+      lift.frontPostUp(1);
+    }
+    
+    if(controller.getAButtonReleased()) {
+      lift.frontPostUp(0);
+    }
+
+    if(controller.getBButton()) {
+      lift.backPostUp(1);
+    }
+    
+    if(controller.getBButtonReleased()) {
+      lift.backPostUp(0);
+    }
+
+    if(controller.getXButton()) {
+      
+    }
+    
+    if(controller.getYButton()) {
+
+    }
+    
+    if(controller.getBumper(Hand.kLeft)) { // Go down
+      pidController.disable();
+      winchMotor.set(-1);
+    }
+    
+    if(controller.getBumperReleased(Hand.kLeft)) {
+      winchMotor.set(0);
+
+    }
+    if(controller.getBumper(Hand.kRight)) { // Go up
+      pidController.enable();
+      winchMotor.set(1);
+    }
+
+    if(controller.getBumperReleased(Hand.kRight)) {
+      winchMotor.set(0);
+    }
+    
+    if(elevatorTopSwitch.get()) {
+      winchMotor.set(0);
+    }
+
+    if(elevatorBottomSwitch.get()) {
+      winchMotor.set(0);
+    }
+
+    if(leftTrigger > 0.0) {
+      lift.liftRobotUp(leftTrigger);
+    }
+
+    if(rightTrigger > 0.0) {
+      lift.backPostDown(rightTrigger);
+    }
+
+    if(dPad == 0) {
+      intake.set(Value.kForward);
+    }
+
+    if(dPad == 180) {
+      intake.set(Value.kReverse);
+    }
+    
+    if(dPad == 90) {
+      intakeSlider.set(Value.kForward);
+    }
+
+    if(dPad == 270) {
+      intakeSlider.set(Value.kReverse);
+    }
+
+    if(controller.getStartButton()) {
+      // vision.startProcessing();
+    }
+
+    if(controller.getBackButton()) {
+      // vision.stop();
+    }
+
+    if(rightJoyStickY != 0) {
+      lift.drive(rightJoyStickY);
+    }
+
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+    compressor.start();
+  }
+}
